003b94aca9bd615676346930dbf63f1253633926
Gergely Imreh <imrehg@gmail.com>
2015-09-22 12:00:27 +0800

    package/genimage: use git version, bump to latest
    Using the latest release 7 of genimage on Arch Linux at the
    moment seems to result in segmentation fault, while it works on
    other distros. The latest git version on the other hand runs
    correctly on all platforms tested. Test was done by running:
    "make wandboard_defconfig && make" as Wandboard is using genimage
    in the post-image script.

--- package/genimage/genimage.mk
+++ package/genimage/genimage.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-GENIMAGE_VERSION = 7
-GENIMAGE_SOURCE = genimage-$(GENIMAGE_VERSION).tar.xz
-GENIMAGE_SITE = http://www.pengutronix.de/software/genimage/download
+GENIMAGE_VERSION = f23b67b7e3c57712c22e226742566e206cfb43be
+GENIMAGE_SITE = git://git.pengutronix.de/git/genimage.git
 GENIMAGE_DEPENDENCIES = host-pkgconf libconfuse
 GENIMAGE_LICENSE = GPLv2
+GENIMAGE_AUTORECONF = YES
 
 $(eval $(host-autotools-package))
